877c40667460938e6bdcb55b90331996b95ef7cd
Rick van den Bosch <rickvdbosch@outlook.com>
2020-09-23 14:04:51 +0200

    Added Functions project

---
+++ ASWA.Example.Functions/BlogPostFunctions.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+
+using ASWA.Example.Common;
+using ASWA.Example.Common.Entities;
+
+namespace ASWA.Example.Functions
+{
+    public static class BlogPostFunctions
+    {
+        [FunctionName(nameof(BlogPostFunctions))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [Table(Constants.TABLENAME_CONTENT, Connection = "scs")]CloudTable cloudTable,
+            ILogger log)
+        {
+            var posts = await GetBlogPostsFromTable(cloudTable);
+
+            return new OkObjectResult(posts);
+        }
+
+        private static async Task<List<BlogPost>> GetBlogPostsFromTable(CloudTable table)
+        {
+            TableQuerySegment<BlogPost> querySegment = null;
+            var entities = new List<BlogPost>();
+            var query = new TableQuery<BlogPost>().Where(TableQuery.GenerateFilterCondition(nameof(ITableEntity.PartitionKey), QueryComparisons.Equal, BlogPost.PARTITIONKEY));
+
+            do
+            {
+                querySegment = await table.ExecuteQuerySegmentedAsync(query, querySegment?.ContinuationToken);
+                entities.AddRange(querySegment.Results);
+            } while (querySegment.ContinuationToken != null);
+
+            return entities;
+        }
+    }
+}
