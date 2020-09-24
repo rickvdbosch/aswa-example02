8a7f0ebad4828337f49cdf1dddf02fca1b000f95
Rick van den Bosch <rickvdbosch@outlook.com>
2020-09-24 21:35:55 +0200

    Update NuGet package here as well (finally, new table binding!)

--- ASWA.Example.Functions/BlogPostFunctions.cs
+++ ASWA.Example.Functions/BlogPostFunctions.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using Microsoft.Azure.Cosmos.Table;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.WindowsAzure.Storage.Table;
 
 using ASWA.Example.Common;
 using ASWA.Example.Common.Entities;
